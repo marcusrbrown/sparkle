fa418e3a94d18490a963d4dc5bd2ca01c7686786
Marcus R. Brown <contact@marcusrbrown.com>
2024-12-21 16:19:25 -0700

    chore: fix issues caught by TypeScript 5.7.2 (#460)

--- packages/fro-jive/app/(tabs)/_layout.tsx
+++ packages/fro-jive/app/(tabs)/_layout.tsx
@@ -30,7 +30,7 @@ export default function TabLayout() {
           title: 'Tab One',
           tabBarIcon: ({color}) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
-            <Link href="/modal" asChild>
+            <Link href="/modal">
               <Pressable>
                 {({pressed}) => (
                   <FontAwesome
