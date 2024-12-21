aae7f89a10008ed6c7a1d4d0d3c32d60c4a138b1
Marcus R. Brown <git@mrbro.dev>
2024-12-21 16:18:47 -0700

    chore: fix issues caught by TypeScript 5.7.2

--- packages/fro-jive/app/+not-found.tsx
+++ packages/fro-jive/app/+not-found.tsx
@@ -1,4 +1,5 @@
 import {Link, Stack} from 'expo-router'
+import React from 'react'
 import {StyleSheet} from 'react-native'
 
 import {Text, View} from '@/components/Themed'
@@ -10,7 +11,7 @@ export default function NotFoundScreen() {
       <View style={styles.container}>
         <Text style={styles.title}>This screen doesn't exist.</Text>
 
-        <Link href="/" style={styles.link}>
+        <Link href="/">
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
       </View>
